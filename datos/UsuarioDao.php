1f6dcbc3e1c75a5a55ca74ed558e5c950d427ca3
eferreyro <emafe7472@gmail.com>
2019-10-16 13:50:40 -0500

    Query de validacion de usuarios en PDO

---
+++ datos/UsuarioDao.php
@@ -0,0 +1,48 @@
+<?php
+//Usuario Data Access Object
+include 'Conexion.php';
+include '../entidades/Usuario.php';
+
+class UsuarioDao extends Conexion
+{
+    protected static $conexion;
+
+    public static function getConexion()
+    {
+        self::$conexion = Conexion::conectar();
+    }
+    private static function desconectar()
+    {
+        self::$conexion = null;
+    }
+    public static function login($usuario)
+    {
+        /**
+         * Metodo que valida el Login
+         * @param object $usuario
+         * @return boolean
+         */
+        $query = "SELECT 
+        id,nombre,usuario,email,privilegio,fecha_registro
+                  FROM usuarios 
+                  WHERE usuario = :usuario 
+                  AND password = :password";
+        
+        self::getConexion();
+        //variable que recibe el resultado del QUERY
+        $resultado = self::$conexion->prepare($query);
+
+        //Paso los parametros de $resultado de la conexion
+        $resultado->bindParam(":usuario", $usuario->getUsuario());
+        $resultado->bindParam(":password", $usuario->getPassword());
+
+        //Ejecuto la conexion PDO ya validada
+        $resultado->execute();
+        
+        // Si el resultado es correcto, retorna TRUE, si no hay usuario retorna FALSE
+        if(count($resultado)){
+            return true;
+        }
+        return false;
+    }
+}
