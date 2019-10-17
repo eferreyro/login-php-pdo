8110439b5c1dc65ee7dddfdb1f4e6d919480f206
eferreyro <emafe7472@gmail.com>
2019-10-17 18:01:10 -0500

    editando helps

--- helps/helps.php
+++ helps/helps.php
@@ -4,7 +4,7 @@
  * @param    input $campo recibe un parametro de entrada de tipo POST
  * @return   string limpio
  * */
-function validar_capo($campo){
+function validar_campo($campo){
     $campo = trim($campo); //trim limpia los campos
     $campo =stripslashes($campo); //impide que pongan barras slash
     $campo = htmlspecialchars($campo); //impide que pongan caracteres html
